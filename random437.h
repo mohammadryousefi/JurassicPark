afa9a981e5fbbbeb43f044307a80891091b961f1
javierz <javierz@unm.edu>
2017-11-02 13:04:57 -0600

    converted c++ to c

--- random437.h
+++ random437.h
@@ -7,7 +7,7 @@
 
 const int max_int = 0x7FFFFFFF;
 
-class Random {
+/*class Random {
   public: 
     Random(bool pseudo = true);
     double random_real();
@@ -18,14 +18,30 @@ class Random {
     int seed;
     int multiplier;
     int add_on;
-};
+    };*/
 
-int Random::reseed() {  
+int pseudo = 1;
+void Random();
+double random_real();
+int random_integer(int low, int high);
+int poisson(double mean);
+
+int reseed();
+int seed;
+int multiplier;
+int add_on;
+
+/*int Random::reseed() {  
     seed = seed * multiplier + add_on;
     return seed;
+    }*/
+int reseed()
+{
+  seed = seed * multiplier + add_on;
+  return seed;
 }
 
-Random::Random(bool pseudo) {
+/*Random::Random(bool pseudo) {
     if (pseudo)
      	seed = 1;
     else {
@@ -34,23 +50,49 @@ Random::Random(bool pseudo) {
 	}
     multiplier = 2743;
     add_on = 5923;
+    }*/
+void Random()
+{
+  if (pseudo)
+    seed = 1;
+  else
+  {
+    srandom(getpid());
+    seed = random();
+  }
+  multiplier = 2743;
+  add_on = 5923;
 }
 
-double Random::random_real() {
+/*double Random::random_real() {
     double max = max_int+1.0;
     double temp = reseed();
     if (temp < 0) temp = temp + max;
     return temp/max;
+    }*/
+double random_real()
+{
+  double max = max_int+1.0;
+  double temp = reseed();
+  if (temp < 0) temp = temp + max;
+  return temp/max;
 }
 
-int Random::random_integer(int low, int high) {
+/*int Random::random_integer(int low, int high) {
     if (low > high) 
        return random_integer(high,low);
     else
        return ((int)((high-low+1)*random_real())) + low;
+       }*/
+int random_integer(int low, int high)
+{
+  if (low > high) 
+    return random_integer(high,low);
+  else
+    return ((int)((high-low+1)*random_real())) + low;
 }
 
-int Random::poisson(double mean) {
+/*int Random::poisson(double mean) {
     double limit = exp(-mean);
     double product = random_real();
     int    count = 0;
@@ -59,6 +101,18 @@ int Random::poisson(double mean) {
 	product *= random_real();
 	}
     return count;
+    }*/
+int poisson(double mean)
+{
+  double limit = exp(-mean);
+  double product = random_real();
+  int    count = 0;
+  while (product > limit)
+  {
+    count++;
+    product *= random_real();
+  }
+  return count;
 }
 
 #endif /* random437_h */
